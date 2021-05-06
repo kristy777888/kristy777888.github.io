048eb15a35925dfc94f2e5948f13cb6b183de49c
kristy777888 <82zhecom@gmail.com>
2021-05-06 10:37:04 +0800

    add link

---
+++ 1.bat
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# build the project
+
+hugo
+
+git add .
+
+msg="rebuilding site `date`"
+
+if [ $# -eq 1 ]
+
+  then msg="$1"
+
+fi
+
+git commit -m "$msg"
+
+# push source to github
+
+git push origin master:master
